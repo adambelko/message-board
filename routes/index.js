4f29514e1e415d4078176edd7c268d100dd822fc
Adzi <belkoadam@gmail.com>
2023-07-31 18:04:02 +1000

    Post method in form now in place, new message is shown in index page along other messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
+    res.render("index", { title: "Message Board", messages });
+});
+
+router.get("/new", (req, res) => {
+    res.render("form");
+});
+
+router.post("/new", (req, res) => {
+    console.log(req.body);
+    messages.unshift({
+        text: req.body.messageText,
+        user: req.body.messageUser,
+        added: new Date(),
+    });
+    res.redirect("/");
 });
 
 module.exports = router;
